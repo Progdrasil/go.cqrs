5f98ee477380ef635ca993fd3dd8a54fcf26ecea
Jet Basrawi <anewexplorer@gmail.com>
2019-03-13 17:25:46 +0000

    Reverted switch to stronly typed

---
+++ util.go
@@ -0,0 +1,27 @@
+package cqrs
+
+import (
+	"reflect"
+
+	"github.com/jetbasrawi/go.cqrs/internal/uuid"
+)
+
+// typeOf is a convenience function that returns the name of a type
+func typeOf(i interface{}) string {
+	return reflect.TypeOf(i).Elem().Name()
+}
+
+// NewUUID returns a new v4 uuid as a string
+func NewUUID() string {
+	return uuid.NewUUID()
+}
+
+// Int returns a pointer to int.
+//
+// There are a number of places where a pointer to int
+// is required such as expectedVersion argument on the repository
+// and this helper function makes keeps the code cleaner in these
+// cases.
+func Int(i int) *int {
+	return &i
+}
