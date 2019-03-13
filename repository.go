364bf8bfbc8e7349d5ec9e5f1e87d6585d53c501
Jet Basrawi <anewexplorer@gmail.com>
2019-03-13 17:55:22 +0000

    Reverted switch to stronly typed

--- repository.go
+++ repository.go
@@ -2,5 +2,5 @@ package cqrs
 
 type Repository interface {
 	Load(aggregateType string, aggregateId string) (Aggregate, error)
-	Save(aggregate Aggregate, expectedVersion *int) error
+	Save(aggregate Aggregate, expectedVersion int) error
 }
